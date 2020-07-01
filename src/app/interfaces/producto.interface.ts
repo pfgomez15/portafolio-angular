20d8f5fcba8fe7e7d2d63173a6fa3a6082beea41
Pablo Gomez <pfgomez15@gmail.com>
2020-07-01 17:44:11 -0500

    Fin de la seccion 6

---
+++ src/app/interfaces/producto.interface.ts
@@ -0,0 +1,6 @@
+export interface ProductoI {
+    categoria: string;
+    cod: string;
+    titulo: string;
+    url: string;
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -6,15 +6,31 @@ import { InfoPaginaI } from '../interfaces/info-pagina.interface';
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info:InfoPaginaI={};
+
+  info: InfoPaginaI = {};
   cargada = false;
+  equipo: any[] = []; 
+
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     //leer archivo json
     this.http.get('assets/data/data-pagina.json')
-    .subscribe((resp:InfoPaginaI)=>{
-      this.cargada=true;
-      this.info=resp;
-      console.log(resp);
+      .subscribe((resp: InfoPaginaI) => {
+        this.cargada = true;
+        this.info = resp;
+        //console.log(resp);
+      })
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-83d43.firebaseio.com/equipo.json')
+    .subscribe((resp: any[]) => {
+        this.equipo = resp;
+        //console.log(resp);
     })
-   }
+  }
 }
